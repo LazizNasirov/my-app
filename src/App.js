f81174669811e54f1514e726ada04d9187e0ca6a
LazizNasirov <laziz.nosirov@gmail.com>
2023-06-04 22:10:48 +0500

    Papkalarga har bir js va css larni kuchirib utildi

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@
 // import Header from './Header.js' alt + shift + strelka vniz kopiya qiladi
 import React from "react";
 import './App.css';
-import Header from './components/Header';
-import Navbar from "./components/Navbar";
-import Profile from "./components/Profile";
+import Header from './components/Header/Header';
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
 
 const App = () => {
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from './Header.module.css';
+
+const Header = () => {
+
+    return <header className={s.header}>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Toggo_plus_Logo_10.2019.svg/1200px-Toggo_plus_Logo_10.2019.svg.png'></img>
+    </header>
+}
+
+export default Header;
