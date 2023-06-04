d229c3ab44819d7b66d2b3257f1003de1955c56d
LazizNasirov <laziz.nosirov@gmail.com>
2023-06-04 21:41:23 +0500

    Header module css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import './Header.css';
+import s from './Header.module.css';
 
 const Header = () => {
 
-    return <header className='header'>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Toggo_plus_Logo_10.2019.svg/1200px-Toggo_plus_Logo_10.2019.svg.png'></img>
-            </header>
+    return <header className={s.header}>
+        <img className={s.header_img} src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Toggo_plus_Logo_10.2019.svg/1200px-Toggo_plus_Logo_10.2019.svg.png'></img>
+    </header>
 }
 
 export default Header;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,9 +4,9 @@ import s from './Profile.module.css';
 
 const Profile = () => {
 
-    return        <div className={s.content}>
+  return <div className={s.content}>
     <div>
-      <img className={s.img1} src='https://cdn.shopify.com/s/files/1/0070/7032/files/best-free-stock-photo-websites.jpg?v=1619036042&width=1024'></img>  
+      <img className={s.img1} src='https://cdn.shopify.com/s/files/1/0070/7032/files/best-free-stock-photo-websites.jpg?v=1619036042&width=1024'></img>
     </div>
     <div>
       <img className={s.img2} src='https://static.vecteezy.com/packs/media/photos/term-bg-3-f6a12264.jpg'></img>
