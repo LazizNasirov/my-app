af02a51548f00977e6208954ccf287e5e3ffb72c
LazizNasirov <laziz.nosirov@gmail.com>
2023-06-06 11:10:13 +0500

    1. Modul install qilindi (npm i react-router-dom -save ) 2. Routerlar bilan ishlandi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/Header/Header';
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const App = () => {
     return (
@@ -15,9 +15,12 @@ const App = () => {
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                {/*<Profile />*/}
-                <Dialogs/>
-
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/dialogs" element={<Dialogs />}/>
+                        <Route path="/profile" element={<Profile />}/>
+                    </Routes>
+                </BrowserRouter>
             </div>
         </div>
     );
